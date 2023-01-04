24f33cefbd42e6d7c392da3d0ad7fa4bdbe9c3bc
Griesser, Nadege <Nadege.Griesser@vector.com>
2023-01-04 19:18:50 +0100

    live debugging

--- src/app/components/resizeable/resizeable.component.ts
+++ src/app/components/resizeable/resizeable.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { FWFService } from 'src/app/services/fwf.service';
 import { AbstractComponent } from '../abstract/abstract.component';
 
@@ -7,6 +7,7 @@ import { AbstractComponent } from '../abstract/abstract.component';
 })
 export abstract class ResizeableComponent extends AbstractComponent implements OnInit {
 
+  @ViewChild('container') div?: ElementRef<HTMLElement>;
   el: HTMLElement;
 
   cols: number = 1;
@@ -31,7 +32,9 @@ export abstract class ResizeableComponent extends AbstractComponent implements O
 
   private resize(): void {
     this.cols = Math.max(1, Math.floor(this.el.offsetWidth / 400));
-    this.rowHeight = Math.max(this.getMinRowHeight(), this.getFactor() / (this.el.offsetWidth / this.cols));
+    if (this.el.offsetWidth > 0) {
+      this.rowHeight = Math.max(this.getMinRowHeight(), this.getFactor() / (this.el.offsetWidth / this.cols));
+    }
     if (this.el.offsetWidth <= 600) {
       this.margin = 0;
     } else {
@@ -40,7 +43,7 @@ export abstract class ResizeableComponent extends AbstractComponent implements O
   }
 
   protected abstract getFactor(): number
-  
+
   protected getMinRowHeight(): number {
     return 300
   }
