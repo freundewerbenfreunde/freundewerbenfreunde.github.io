61218edfb1d49c74910c1aadbb7cc99c4827750c
Griesser, Nadege <Nadege.Griesser@vector.com>
2023-01-05 11:05:16 +0100

    save theme to local storage

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
-  private _darkTheme = new BehaviorSubject<boolean>(false);
-  isDarkTheme = this._darkTheme.asObservable();
+  private _darkTheme: BehaviorSubject<boolean>;
+  isDarkTheme: Observable<boolean>;
+
+  constructor() {
+    let theme = localStorage.getItem('theme')
+    this._darkTheme = new BehaviorSubject<boolean>(theme == 'dark');
+    this.isDarkTheme = this._darkTheme.asObservable();
+  }
 
   setDarkTheme(isDarkTheme: boolean): void {
     this._darkTheme.next(isDarkTheme);
+    localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
   }
 }
